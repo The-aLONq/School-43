ede1ed5bdab1424a282d89f39d0fc6544fec682c
The-aLONq <thealonq@mail.ru>
2023-11-28 16:50:56 +0500

    final

--- Code_s/JavaScript/script.js
+++ Code_s/JavaScript/script.js
@@ -2,7 +2,24 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     items: 1,
     dots: true,
-    nav: true
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    loop: true,
+    smartSpeed: 1000
+  });
+});
+let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+    if (this.checked) {
+      checkboxes.forEach(cb => {
+        if (cb !== this) {
+          cb.checked = false;
+        }
+      });
+    }
   });
 });
 
